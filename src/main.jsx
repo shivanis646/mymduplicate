3460463d4915ee604cd7ccdaf521806bb31ae240
Shivani <harinakshi0604@gmail.com>
2025-09-23 15:07:54 +0530

    router change

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "leaflet/dist/leaflet.css";
-
+import { BrowserRouter } from "react-router-dom"; // ✅ add this
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>
 );
